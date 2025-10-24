310e38b09d87e7153d695a6367ff84fa94f54d66
Maroof Shah <shahmaroof2006@gmail.com>
2025-10-24 13:11:33 +0100

    Create window and set background colour

--- tech-demo/src/main.c
+++ tech-demo/src/main.c
@@ -3,7 +3,27 @@
 
 int main(void)
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 450;
 
+    // initialise window
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cave Game");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+        // update things here
+
+        // draw
+        BeginDrawing();
+
+        ClearBackground(DARKGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
