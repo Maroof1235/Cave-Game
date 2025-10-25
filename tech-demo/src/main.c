d798d7ab83944b7f99df266d2d827f16de5f68e3
Maroof Shah <shahmaroof2006@gmail.com>
2025-10-25 16:31:19 +0100

    Drawing 2d shape to window

--- tech-demo/src/main.c
+++ tech-demo/src/main.c
@@ -3,22 +3,36 @@
 
 int main(void)
 {
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 450;
+    const int WIDTH = 800;
+    const int HEIGHT = 800;
 
     // initialise window
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cave Game");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(WIDTH, HEIGHT, "Cave Game");
+
+
+
 
     SetTargetFPS(60);
 
     while(!WindowShouldClose())
     {
         // update things here
+        int SCREEN_WIDTH;
+        int SCREEN_HEIGHT;
+
+        if(IsWindowResized)
+        {
+            SCREEN_WIDTH = GetScreenWidth();
+            SCREEN_HEIGHT = GetScreenHeight();
+        }
 
         // draw
         BeginDrawing();
 
         ClearBackground(DARKGRAY);
+
+        DrawRectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,20 , 20, RED);
 
         EndDrawing();
     }
