568010d292c1d1ddb5d8a0cd13b7cc0374278dfd
Maroof Shah <shahmaroof2006@gmail.com>
2025-10-26 16:37:12 +0000

    Added left and right movement

--- tech-demo/src/main.c
+++ tech-demo/src/main.c
@@ -1,38 +1,51 @@
 #include "raylib.h"
 
+typedef struct {
+    Vector2 player_size;
+    Vector2 player_pos;
+    Color player_colour;
+} Player;
 
 int main(void)
 {
-    const int WIDTH = 800;
-    const int HEIGHT = 800;
+    const int SCREEN_WIDTH = 1600;
+    const int SCREEN_HEIGHT = 900;
 
     // initialise window
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(WIDTH, HEIGHT, "Cave Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cave Game");
 
+    Player p;
+    p.player_size.x = (float)50;
+    p.player_size.y = (float)50;
 
+    p.player_pos.x = (float)SCREEN_WIDTH/2;
+    p.player_pos.y = SCREEN_HEIGHT - p.player_size.x;
 
+    p.player_colour = BLUE;
 
     SetTargetFPS(60);
 
+
+    // main game loop
     while(!WindowShouldClose())
     {
         // update things here
-        int SCREEN_WIDTH;
-        int SCREEN_HEIGHT;
 
-        if(IsWindowResized)
+        if (IsKeyDown(KEY_A))
         {
-            SCREEN_WIDTH = GetScreenWidth();
-            SCREEN_HEIGHT = GetScreenHeight();
+            p.player_pos.x -= 4.0f;
+        }
+        if (IsKeyDown(KEY_D))
+        {
+            p.player_pos.x += 4.0f;
         }
 
         // draw
         BeginDrawing();
-
+  
         ClearBackground(DARKGRAY);
 
-        DrawRectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,20 , 20, RED);
+        DrawRectangleV(p.player_pos, p.player_size, p.player_colour);
 
         EndDrawing();
     }
